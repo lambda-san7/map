c10ac78081957dda0a72a8c1c1855497dba7027b
lambda-san7 <codekiwi132@gmail.com>
2023-06-26 13:48:35 -0700

    i hate git so fucking much ahhhhh

---
+++ script.js
@@ -0,0 +1,68 @@
+var color = "green"
+var code = 4
+
+var map = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+]
+
+function clicked(button){
+    button.style.backgroundColor = color;
+    map[button.id.slice(0,button.id.indexOf(","))][button.id.slice(button.id.indexOf(",") + 1)] = code
+}
+
+function button(x,y){
+    grid = document.getElementById("grid")
+    div = document.createElement("div")
+    div.classList.add("square")
+    div.onclick = function() {
+        clicked(this)
+    }
+    div.id = `${x},${y}`
+
+    grid.appendChild(div)
+}
+
+for (let i = 0; i < 20; i++) {
+    for (let j = 0; j < 20; j++) {
+      button(j, i);
+    }
+  }
+
+load = function(){
+    fullmap = ""
+    for (let i in map) {
+        row = ""
+        for (let j in map[i]){
+            row = row.concat(map[j][i],", ");
+        }
+        console.log(`[${row}],`)
+        fullmap = fullmap.concat(`[${row}],`,"\n");
+    }
+    console.log(fullmap)
+    document.getElementById("mapout").innerHTML = fullmap
+}
+
+
+change_pen = function(clr,num){
+    color = clr
+    code = num
+}
